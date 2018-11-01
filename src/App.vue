2d1d63fd6d526a72fa8ce9bac573ba293a01015b
dgershom <dgershom@gmail.com>
2018-11-01 01:18:15 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+  
+      <v-toolbar-title class="white--text">DoughSavers</v-toolbar-title>
+  
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>fa-search</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
