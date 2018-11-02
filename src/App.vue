b8407f7b010d2c482fcbe13de8d01fb26c8461d7
dgershom <dgershom@gmail.com>
2018-11-02 01:01:14 -0400

    Initial Nav Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,54 @@
 <template>
   <v-app>
-    <v-toolbar app dark color="red darken-4">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-navigation-drawer
+      :clipped="clipped" 
+      v-model="drawer" 
+      enable-resize-watcher 
+      app 
+      dark
+      class="green darken-4"
+    >
+      <v-toolbar 
+      class="green darken-4"      
+      >
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title green darken-4">
+              
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+  
+      <v-divider></v-divider>
+      
+      <v-list>
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+  
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar 
+      fixed
+      app 
+      :clipped-left="clipped" 
+      dark 
+      color="red darken-4"
+      >
+
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
      
        <v-flex xs12>
         <v-img
@@ -30,7 +77,13 @@ export default {
   name: 'App',
   data () {
     return {
-      //
+      drawer: true,
+      clipped: false,
+      items: [
+        { title: 'Dashboard', icon: 'fa-adn' },
+        { title: 'Account', icon: 'fa-list' },
+        { title: 'Admin', icon: 'fa-edit' }
+      ]
     }
   }
 }
