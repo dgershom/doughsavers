3022c5c9697b4afb4c0e019e70386fce2ca58daa
dgershom <dgershom@gmail.com>
2018-11-04 19:41:15 -0500

    Initial Drawer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,14 +4,7 @@
       text-xs-center
       wrap
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.png')"
-          class="my-3"
-          contain
-          height="300"
-        ></v-img>
-      </v-flex>
+     +
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
